bee2e1f9eb11d8f3406a0d27d59dcfff57da86c1
Claudia Chu <cchu@broadinstitute.org>
2025-05-04 00:08:22 -0400

    different normalization for ATAC for PCA and running poincaremap with more epochs

---
+++ scripts/run_poincare_map.py
@@ -0,0 +1,76 @@
+import sys
+sys.path.append('/Users/cchu/Desktop/phd_work/hyperChromatin/src/PoincareMaps')
+
+import scanpy as sc
+import pandas as pd
+import numpy as np
+from refs import celltype_colors
+import matplotlib.pyplot as plt
+from IPython.display import Image
+import multiprocessing as mp
+import argparse
+import tqdm
+from main import *
+from poincare_maps import *
+
+
+rna_pca_name = '../results/02/rna_pca.celltype_label'
+rna_simba_name = '../results/02/rna_simba.celltype_label'
+atac_pca_name = '../results/02/atac_pca.celltype_label'
+atac_std_pca_name = '../results/02/atac_std_pca.celltype_label'
+atac_simba_name = '../results/02/atac_simba.celltype_label'
+simba_multi_name = '../results/02/simba_multi.celltype_label'
+
+def run_poincare_map(file_name, output_name, epochs=1000, lr=0.1):
+    print(f'Running {file_name}...')
+    features, labels = prepare_data(file_name, 
+                                with_labels=True, 
+                                normalize=True, 
+                                n_pca=0)
+    
+    poincare_coord, _ = compute_poincare_maps(features, labels,
+                        output_name,
+                        mode='features', k_neighbours=15, 
+                        distlocal='minkowski', sigma=1.0, gamma=2.0,
+                        color_dict=celltype_colors, epochs=epochs,
+                        batchsize=32, lr=lr, earlystop=0.0001, cuda=0)
+    
+    poincare_coord_df = pd.DataFrame(np.concatenate([poincare_coord, labels.reshape(-1, 1)], axis=1))
+    poincare_coord_df.columns = ['x', 'y', 'labels']
+    poincare_coord_fn = f'{output_name}.labeled.csv'
+    poincare_coord_df.to_csv(poincare_coord_fn, index=False, sep=',')
+    
+    model = PoincareMaps(poincare_coord)
+    model.plot('ori', labels=labels, file_name=output_name, 
+           title_name='Poincaré map',
+           coldict=celltype_colors, 
+           labels_order=None, 
+           zoom=4, bbox=(1.1, 0.8), leg=False, ft='png')
+    
+    return poincare_coord_fn
+    
+
+if __name__ == '__main__':
+    # Run from scripts folder
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--epochs', type=int, default=5)
+    parser.add_argument('--lr', type=float, default=0.1)
+    args = parser.parse_args()
+
+    file_names = [
+        rna_pca_name, rna_simba_name, 
+        atac_pca_name, atac_std_pca_name,atac_simba_name, simba_multi_name
+    ]
+    epochs = args.epochs
+    lr = args.lr
+    output_names = [f'../results/03/{file_name.split("/")[-1]}.ep{epochs}_lr{lr}.poincare_coord' for file_name in file_names]
+
+    poincare_coord_fns = {}
+
+    for i, (file_name, output_name) in tqdm.tqdm(enumerate(zip(file_names, output_names)), total=len(file_names)):
+        result = run_poincare_map(file_name, output_name, epochs, lr)
+        poincare_coord_fns[file_names[i]] = result
+
+
+    pd.DataFrame(poincare_coord_fns, index=[0]).to_csv('../results/03/poincare_coord_fns.csv', index=False)
---
+++ scripts/tfidf.py
@@ -0,0 +1,133 @@
+"""
+Copied from https://github.com/scverse/muon/blob/bc6c53069febd28866ad4f5ed0c2187a55d8fa30/muon/_atac/preproc.py#L16
+
+MUON: multimodal omics analysis framework
+
+Danila Bredikhin, Ilia Kats, Oliver Stegle
+
+Genome Biology 2022 Feb 01. doi: 10.1186/s13059-021-02577-8.
+
+"""
+
+from typing import Union, Optional
+from warnings import warn
+
+import numpy as np
+from scipy.sparse import csr_matrix, dia_matrix, issparse
+
+from anndata import AnnData
+
+from scanpy._utils import view_to_actual
+
+def tfidf(
+    data: Union[AnnData],
+    log_tf: bool = True,
+    log_idf: bool = True,
+    log_tfidf: bool = False,
+    scale_factor: Union[int, float] = 1e4,
+    inplace: bool = True,
+    copy: bool = False,
+    from_layer: Optional[str] = None,
+    to_layer: Optional[str] = None,
+):
+    """
+    Transform peak counts with TF-IDF (Term Frequency - Inverse Document Frequency).
+
+    TF: peak counts are normalised by total number of counts per cell
+    DF: total number of counts for each peak
+    IDF: number of cells divided by DF
+
+    By default, log(TF) * log(IDF) is returned.
+
+    Parameters
+    ----------
+    data
+            AnnData object with peak counts or multimodal MuData object with 'atac' modality.
+    log_idf
+            Log-transform IDF term (True by default).
+    log_tf
+            Log-transform TF term (True by default).
+    log_tfidf
+            Log-transform TF*IDF term (False by default).
+            Can only be used when log_tf and log_idf are False.
+    scale_factor
+            Scale factor to multiply the TF-IDF matrix by (1e4 by default).
+    inplace
+            If to modify counts in the AnnData object (True by default).
+    copy
+            If to return a copy of the AnnData object or the 'atac' modality (False by default).
+            Not compatible with inplace=False.
+    from_layer
+            Layer to use counts (AnnData.layers[from_layer])
+            instead of AnnData.X used by default.
+    to_layer
+            Layer to save transformed counts to (AnnData.layers[to_layer])
+            instead of AnnData.X used by default.
+            Not compatible with inplace=False.
+    """
+    if isinstance(data, AnnData):
+        adata = data
+    else:
+        raise TypeError("Expected AnnData or MuData object with 'atac' modality")
+
+    if log_tfidf and (log_tf or log_idf):
+        raise AttributeError(
+            "When returning log(TF*IDF), \
+            applying neither log(TF) nor log(IDF) is possible."
+        )
+
+    if copy and not inplace:
+        raise ValueError("`copy=True` cannot be used with `inplace=False`.")
+
+    if to_layer is not None and not inplace:
+        raise ValueError(f"`to_layer='{str(to_layer)}'` cannot be used with `inplace=False`.")
+
+    if copy:
+        adata = adata.copy()
+
+    view_to_actual(adata)
+
+    counts = adata.X if from_layer is None else adata.layers[from_layer]
+
+    # Check before the computation
+    if to_layer is not None and to_layer in adata.layers:
+        warn(f"Existing layer '{str(to_layer)}' will be overwritten")
+
+    if issparse(counts):
+        n_peaks = np.asarray(counts.sum(axis=1)).reshape(-1)
+        n_peaks = dia_matrix((1.0 / n_peaks, 0), shape=(n_peaks.size, n_peaks.size))
+        # This prevents making TF dense
+        tf = np.dot(n_peaks, counts)
+    else:
+        n_peaks = np.asarray(counts.sum(axis=1)).reshape(-1, 1)
+        tf = counts / n_peaks
+
+    if scale_factor is not None and scale_factor != 0 and scale_factor != 1:
+        tf = tf * scale_factor
+    if log_tf:
+        tf = np.log1p(tf)
+
+    idf = np.asarray(adata.shape[0] / counts.sum(axis=0)).reshape(-1)
+    if log_idf:
+        idf = np.log1p(idf)
+
+    if issparse(tf):
+        idf = dia_matrix((idf, 0), shape=(idf.size, idf.size))
+        tf_idf = np.dot(tf, idf)
+    else:
+        tf_idf = np.dot(csr_matrix(tf), csr_matrix(np.diag(idf)))
+
+    if log_tfidf:
+        tf_idf = np.log1p(tf_idf)
+
+    res = np.nan_to_num(tf_idf, nan=0.0)
+    if not inplace:
+        return res
+
+    if to_layer is not None:
+        adata.layers[to_layer] = res
+    else:
+        adata.X = res
+
+    if copy:
+        return adata
